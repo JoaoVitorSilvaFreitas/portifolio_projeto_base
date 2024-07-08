eb4083cdd72f2882f9b3a6402f3b44f0627a409f
João Vitor <joaovitordasilvafreitas@gmail.com>
2024-07-08 18:16:52 -0300

    Adição da função de fechar ao clicar fora da sidebar

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
-function openNav() {
+function abrirNav() {
     document.getElementById("sidebar").style.width = "250px";
+    setTimeout(() => {
+        document.addEventListener('click', clickFora);
+    }, 0); // Adiciona um pequeno atraso para evitar o conflito de clique imediato
 }
 
-function closeNav() {
+function fecharNav() {
     document.getElementById("sidebar").style.width = "0";
+    document.removeEventListener('click', clickFora);
+}
+
+function clickFora(event) {
+    var sidebar = document.getElementById("sidebar");
+    if (!sidebar.contains(event.target) && sidebar.style.width === "250px") {
+        fecharNav();
+    }
 }
