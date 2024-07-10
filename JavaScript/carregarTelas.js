e2e272d15b0d79e26a9784cb5df646f921a404e7
João Vitor <joaovitordasilvafreitas@gmail.com>
2024-07-10 09:40:06 -0300

    Adição de alteração dinâmica nas telas usando JS e também algumas organizações no projeto

--- JavaScript/carregarTelas.js
+++ JavaScript/carregarTelas.js
@@ -14,7 +14,25 @@ function loadHTML(element, file) {
         });
 }
 
+// VERIFICA SE O CONTEUDO MAIN ESTÁ CARREGADO NA TELA QUANDO SE CLICA
+function loadContent(url) {
+    fetch(url)
+        .then(response => response.text())
+        .then(data => {
+            const mainContainer = document.querySelector('#conteudo_main');
+            if (mainContainer) {
+                console.log('Elemento encontrado:', mainContainer);
+                mainContainer.innerHTML = data;
+            } else {
+                console.error('Elemento #conteudo_main não encontrado');
+            }
+        })
+        .catch(error => console.error('Erro ao carregar o conteúdo:', error));
+}   
+
 document.addEventListener("DOMContentLoaded", function() {
     loadHTML("header", "./HTML/header.html");
     loadHTML("aside", "./HTML/aside.html");
+    loadHTML("main","../HTML/conteudoPrincipal.html")
 });
+
