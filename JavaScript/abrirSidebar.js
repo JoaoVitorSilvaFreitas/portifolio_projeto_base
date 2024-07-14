9e9587fe1843d60cafd19f79505102dc98fb5e47
João Vitor <joaovitordasilvafreitas@gmail.com>
2024-07-14 10:00:36 -0300

    Função de fechar sidebar ao clicar no icone

--- JavaScript/abrirSidebar.js
+++ JavaScript/abrirSidebar.js
@@ -5,18 +5,28 @@ function abrirNav() {
     setTimeout(() => {
         document.addEventListener('click', clickFora);
     }, 0);
+
+    var sidebarLinks = document.querySelectorAll('#sidebar a');
+    sidebarLinks.forEach(function(link) {
+        link.addEventListener('click', fecharNav);
+    });
 }
 
 function fecharNav() {
     document.getElementById("sidebar").classList.remove("sidebar-open");
     document.body.classList.remove("body-lock");
     document.removeEventListener('click', clickFora);
+
+    var sidebarLinks = document.querySelectorAll('#sidebar a');
+    sidebarLinks.forEach(function(link) {
+        link.removeEventListener('click', fecharNav);
+    });
 }
 
 function clickFora(event) {
     var sidebar = document.getElementById("sidebar");
-
     if (!sidebar.contains(event.target) && sidebar.classList.contains("sidebar-open")) {
         fecharNav();
     }
+    
 }
